ae137a04df064491f5d3fcd4078a18c71ecc0d9d
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-10 22:52:47 +0530

    Update kora.py

--- kora.py
+++ kora.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("rm -rf Kayn && git clone https://github.com/AbhiMod/Kayn && cd Kayn && python3 -m Yone")
+system ("rm -rf Kayn && git clone https://github.com/AMOPBOT/Kayn && cd Kayn && python3 -m Yone")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
